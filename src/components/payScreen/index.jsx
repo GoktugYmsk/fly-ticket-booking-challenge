3d192015843d832190b5f9298007f74822aa99d0
GoktugYmsk <gogtug033@outlook.com>
2023-07-19 11:35:59 +0300

    pay screen is edited

--- src/components/payScreen/index.jsx
+++ src/components/payScreen/index.jsx
@@ -1,25 +1,32 @@
 import React, { useState, useEffect } from 'react';
-import Button from 'react-bootstrap/Button';
 import { useSelector } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
+import Toast from 'react-bootstrap/Toast';
+import Button from '@mui/material/Button';
 import './index.scss';
 
 function PayScreen() {
-  const [cardNumber, setCardNumber] = useState('');
-  const [maskedCardNumber, setMaskedCardNumber] = useState('################');
-  const [expiryMonth, setExpiryMonth] = useState('');
-  const [expiryYear, setExpiryYear] = useState('');
-  const [cardName, setCardName] = useState('');
   const [cvv, setCVV] = useState('');
+  const [cardName, setCardName] = useState('');
+  const [expiryYear, setExpiryYear] = useState('');
+  const [cardNumber, setCardNumber] = useState('');
   const [isFlipped, setIsFlipped] = useState(false);
+  const [popup, setPopup] = useState(false)
+  const [popupActive, setPopupActive] = useState(false)
+  const [expiryMonth, setExpiryMonth] = useState('');
+  const [selectedOption, setSelectedOption] = useState('');
+  const [maskedCardNumber, setMaskedCardNumber] = useState('################');
 
   const flightTicket = useSelector((state) => state.passTicket.flightTicket);
+
+  const navigate = useNavigate()
 
   const totalPassenger = sessionStorage.getItem('totalPassenger')
 
   const totalPrice = flightTicket.priceDetail.basePrice.amount * totalPassenger
 
   useEffect(() => {
-    if (cardNumber.length === 16 && expiryMonth !== '' && expiryYear !== '' && cardName === 'Göktuğ Yumuşak') {
+    if (cardNumber.length === 16 && expiryMonth !== '' && expiryYear !== '' && cardName === 'Fly Pinsoft') {
       setIsFlipped(true);
     } else {
       setIsFlipped(false);
@@ -73,69 +80,150 @@ function PayScreen() {
 
   const handleApprovalClick = () => {
     if (cvv === '001') {
-      alert('Ödeme Başarıyla Tamamlandı')
+      setPopupActive(true)
+      setPopup(true)
     }
   }
 
 
-  return (
-    <div className="payScreen-container">
-      <div className="payScreen-container-cardForm">
-        <div className={`payScreen-card ${isFlipped ? 'flipped' : ''}`}>
-          <div className="payScreen-card__front">
-            <div>
-              <label>Kart Numarası:</label>
-              <p className="payScreen-card__front-number">{formatCardNumber(maskedCardNumber)}</p>
-            </div>
+  const handleMainPage = () => {
+    setPopupActive(false)
+    navigate('/')
+  }
 
-            <p>{cardName}</p>
-            <div>
-              <label>Son Kullanma Tarihi:</label>
-              <p>{expiryMonth}/{expiryYear}</p>
-            </div>
+
+  const logo =
+    'https://uploads-ssl.webflow.com/605c9d764f1ef938a009ac98/61e01bfbdd8632a72962edc2_Pinsoft_Yatay_Logo_mavi-for%20animation.svg';
+
+  return (
+    <>
+      <div className={`payScreen__container ${popupActive ? 'payScreen__container-opacity' : 'payScreen__container'}`} >
+        <div className="payScreen__container-navbar" >
+          <nav >
+            <img onClick={handleMainPage} src={logo} />
+          </nav>
+          <div className='payScreen__container-navbar-alt' >
+            <h3>Ödeme Bilgileri</h3>
           </div>
-          <div className="payScreen-card__back">
-            <div>
-              <div className="payScreen-card__back__bant" ></div>
-              <input className="payScreen-card__back-input" type="text" placeholder="cvv" value={cvv} onChange={(e) => setCVV(e.target.value)} maxLength="3" />
+        </div>
+
+        <div className="payScreen-container-list">
+          <div className="payScreen-container-box">
+            <div className="payScreen-container-cardForm">
+              <div className={`payScreen-card ${isFlipped ? 'flipped' : ''}`}>
+                <div className="payScreen-card__front">
+                  <div>
+                    <label>Kart Numarası:</label>
+                    <p className="payScreen-card__front-number">{formatCardNumber(maskedCardNumber)}</p>
+                  </div>
+
+                  <p>{cardName}</p>
+                  <div>
+                    <label>Son Kullanma Tarihi:</label>
+                    <p>{expiryMonth}/{expiryYear}</p>
+                  </div>
+                </div>
+                <div className="payScreen-card__back">
+                  <div>
+                    <div className="payScreen-card__back__bant" ></div>
+                    <input className="payScreen-card__back-input" type="text" placeholder="cvv" value={cvv} onChange={(e) => setCVV(e.target.value)} maxLength="3" />
+                  </div>
+                </div>
+              </div>
+              {!isFlipped ? (
+                <div className='payScreen-container-cardForm-frontContent' >
+                  <input type="text" value={formatCardNumber(cardNumber)} onChange={handleCardNumberChange} placeholder="Kart Numarası" maxLength="19" />
+                  <input type="text" value={cardName} onChange={handleCardName} placeholder="Kart Üzerindeki İsim" />
+                  <div>
+                    <select value={expiryMonth} onChange={handleExpiryMonthChange}>
+                      <option value="">Ay</option>
+                      {Array.from({ length: 12 }, (_, index) => index + 1).map((month) => (
+                        <option value={month.toString().padStart(2, '0')} key={month}>
+                          {month.toString().padStart(2, '0')}
+                        </option>
+                      ))}
+                    </select>
+                    <select value={expiryYear} onChange={handleExpiryYearChange}>
+                      <option value="">Yıl</option>
+                      {generateYears().map((year) => (
+                        <option value={year} key={year}>
+                          {year}
+                        </option>
+                      ))}
+                    </select>
+                  </div>
+                </div>
+              ) : (
+                <div className='payScreen-container-cvvSide' >
+                  <input type="text" placeholder="cvv" value={cvv} onChange={(e) => setCVV(e.target.value)} maxLength="3" />
+                </div>
+              )}
+              <div className='payScreen-container-box__installment' >
+                <h3>Taksit Seçenekleri</h3>
+                <div className='payScreen-container-box__installment-inputGroup'>
+                  <div>
+                    <input
+                      type='radio'
+                      checked={selectedOption === 'option1'}
+                      onChange={() => setSelectedOption('option1')}
+                    />
+                    <p>Tek çekim</p>
+                  </div>
+                  <div>
+                    <input
+                      type='radio'
+                      checked={selectedOption === 'option2'}
+                      onChange={() => setSelectedOption('option2')}
+                    />
+                    <p>3 Ay</p>
+                  </div>
+                  <div>
+                    <input
+                      type='radio'
+                      checked={selectedOption === 'option3'}
+                      onChange={() => setSelectedOption('option3')}
+                    />
+                    <p>6 Ay</p>
+                  </div>
+                  <div>
+                    <input
+                      type='radio'
+                      checked={selectedOption === 'option4'}
+                      onChange={() => setSelectedOption('option4')}
+                    />
+                    <p>9 Ay</p>
+                  </div>
+                </div>
+              </div>
+              <div className='payScreen-container-cardForm-check' >
+                <input type='checkbox' />
+                <p>Pinsoft işlem kurallarını okudum ve Kabul ediyorum</p>
+              </div>
+              <div className='payScreen-container__paySide' >
+                <h3 className='payScreen-container-h3' >Toplam ödenecek tutar {totalPrice} $</h3>
+                <Button className='approvalButton' onClick={handleApprovalClick} variant='secondary'>Ödemeyi Tamamla</Button>
+              </div>
             </div>
           </div>
         </div>
-        {!isFlipped ? (
-          <div>
-            <input type="text" value={formatCardNumber(cardNumber)} onChange={handleCardNumberChange} maxLength="19" />
-            <div>
-              <select value={expiryMonth} onChange={handleExpiryMonthChange}>
-                <option value="">Ay</option>
-                {Array.from({ length: 12 }, (_, index) => index + 1).map((month) => (
-                  <option value={month.toString().padStart(2, '0')} key={month}>
-                    {month.toString().padStart(2, '0')}
-                  </option>
-                ))}
-              </select>
-              <select value={expiryYear} onChange={handleExpiryYearChange}>
-                <option value="">Yıl</option>
-                {generateYears().map((year) => (
-                  <option value={year} key={year}>
-                    {year}
-                  </option>
-                ))}
-              </select>
-            </div>
-            <input type="text" value={cardName} onChange={handleCardName} placeholder="Kart Üzerindeki İsim" />
-          </div>
-        ) : (
-          <div className='payScreen-container-cvvSide' >
-            <input type="text" placeholder="cvv" value={cvv} onChange={(e) => setCVV(e.target.value)} maxLength="3" />
-            <Button className='approvalButton' onClick={handleApprovalClick} variant='secondary'>Onaylıyor musunuz ?</Button>
-          </div>
-        )}
-        <h3 className='payScreen-container-h3' >Toplam {totalPrice} $</h3>
       </div>
-      <div className='payScreen-container-total' >
-
-      </div>
-    </div>
+      {popup && (
+        <div className="toast-container">
+          <Toast onClose={() => setPopup(false)} show={popup} >
+            <Toast.Body>
+              <div className='popup-content' >
+                <p>Satın alma işlemini başarıyla tamamladınız!</p>
+                <p>PNR NO: A812N</p>
+                <div className='popup-content-button' >
+                  <button onClick={handleMainPage} >Anasayfaya Dön</button>
+                  <button  >uçuş Bilgilerimi Görüntüle</button>
+                </div>
+              </div>
+            </Toast.Body>
+          </Toast>
+        </div>
+      )}
+    </>
   );
 }
 
