aa03d9bd39589b380b209cd947095a471b0a8535
GoktugYmsk <gogtug033@outlook.com>
2023-07-20 10:39:14 +0300

    shoppingSummary component is added

---
+++ src/components/content/shoppingSummary/index.jsx
@@ -0,0 +1,65 @@
+import React from 'react'
+import { useSelector } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
+
+import './index.scss'
+
+function ShoppingSummary() {
+
+    const flightTicket = useSelector((state) => state.passTicket.flightTicket);
+    const passengerInfo = useSelector((state) => state.passInfo.passengerInfo);
+    const passSurname = useSelector((state) => state.passCheck.passSurname);
+
+    const navigate = useNavigate()
+
+    console.log('flightTicket', flightTicket)
+
+    const totalPassenger = sessionStorage.getItem('totalPassenger')
+
+
+    const logo =
+        'https://uploads-ssl.webflow.com/605c9d764f1ef938a009ac98/61e01bfbdd8632a72962edc2_Pinsoft_Yatay_Logo_mavi-for%20animation.svg';
+
+    const handleMainPage = () => {
+        navigate('/')
+    }
+
+    return (
+        <div className='shoppingSummary-container' >
+            <div className="shoppingSummary__container-navbar" >
+                <nav >
+                    <img onClick={handleMainPage} src={logo} />
+                </nav>
+                <div className='shoppingSummary__container-navbar-alt' >
+                    <h3>Özet</h3>
+                </div>
+            </div>
+            <div className='shoppingSummary-container-box' >
+                <h4 onClick={handleMainPage} >Anasayafa Dön</h4>
+                <div className='shoppingSummary-container-box__list' >
+                    <div className='shoppingSummary-container-box__list-top' >
+                        <p>{flightTicket.airline}</p>
+                        <p>{flightTicket.flightNo}</p>
+                        <p>{flightTicket.depTime}</p>
+                        <p>{flightTicket.arrTime}</p>
+                        <p> Toplam: {totalPassenger} Yolcu </p>
+                    </div>
+
+                    <div className='shoppingSummary-container-box__list-top' >
+                        <p>{passengerInfo.adults} Yetişkin </p>
+                        {passengerInfo.children > 0 &&
+                            <p> {`  - ${passengerInfo.children}  Çocuk `}</p>
+                        }
+                        {passengerInfo.babies > 0 &&
+                            <p> {` - ${passengerInfo.babies}  Bebek`} </p>
+
+                        }
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    )
+}
+
+export default ShoppingSummary
