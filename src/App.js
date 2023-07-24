738924eff0bd1bb6799cabec2525e6b427964c9c
GoktugYmsk <gogtug033@outlook.com>
2023-07-24 13:47:08 +0300

    firebase added

--- src/App.js
+++ src/App.js
@@ -1,4 +1,7 @@
-import React from 'react';
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getDatabase, ref, onValue } from "firebase/database";
+import React, { useEffect } from 'react';
 import { Route, Routes } from 'react-router-dom';
 import CustomComponent from './components/CustomComponent';
 import SearchContent from './components/content/searchContent';
@@ -11,7 +14,27 @@ import SalesScreen from './components/salesScreen';
 import ShoppingSummary from './components/content/shoppingSummary';
 import './App.css'
 
+const firebaseConfig = {
+  apiKey: "AIzaSyDZKTi2vfFnzkNJlp7ezobTlpG5YJIOE78",
+  authDomain: "fly-database-2cf87.firebaseapp.com",
+  databaseURL: "https://fly-database-2cf87-default-rtdb.firebaseio.com",
+  projectId: "fly-database-2cf87",
+  storageBucket: "fly-database-2cf87.appspot.com",
+  messagingSenderId: "774953044650",
+  appId: "1:774953044650:web:a2927d8fd36b0c84d05ed3"
+};
+
+const app = initializeApp(firebaseConfig);
+
 function App() {
+  const database = getDatabase();
+
+  const portsRef = ref(database, "ports");
+  onValue(portsRef, (snapshot) => {
+    const portsData = snapshot.val();
+    console.log(portsData);
+  });
+
   return (
     <div className="App">
       <Routes>
@@ -25,7 +48,7 @@ function App() {
         <Route path="/sales-screen" element={<SalesScreen />} />
         <Route path="/shopping-summary" element={<ShoppingSummary />} />
       </Routes>
-    </div >
+    </div>
   );
 }
 
---
+++ src/firebase.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyDZKTi2vfFnzkNJlp7ezobTlpG5YJIOE78",
+    authDomain: "fly-database-2cf87.firebaseapp.com",
+    databaseURL: "https://fly-database-2cf87-default-rtdb.firebaseio.com",
+    projectId: "fly-database-2cf87",
+    storageBucket: "fly-database-2cf87.appspot.com",
+    messagingSenderId: "774953044650",
+    appId: "1:774953044650:web:a2927d8fd36b0c84d05ed3"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
