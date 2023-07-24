d022353033bd1974745565707d652ca5e9732b78
Melike Kizilcik <melikeekizilcikk@gmail.com>
2023-07-24 21:43:37 +0300

    companies ve shopping summary eklendi

--- src/components/componies/index.jsx
+++ src/components/componies/index.jsx
@@ -1,140 +1,188 @@
-import React, { useEffect, useState } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import passengerInformation from '../../assets/passenger';
-import flightPorts from '../../assets/flightPorts';
-import { setFlightTicket } from '../configure';
-import { setPassengerInfo } from '../configure';
-import './index.scss';
-import { useNavigate } from 'react-router-dom';
+import React, { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import passengerInformation from "../../assets/passenger";
+import flightPorts from "../../assets/flightPorts";
+import { setFlightTicket } from "../configure";
+import { setPassengerInfo } from "../configure";
+import "./index.scss";
+import { useNavigate } from "react-router-dom";
 
 function FlyCompanies() {
+  const [formattedSelectedDate, setFormattedSelectedDate] = useState("");
+  const [formattedReturnDate, setFormattedReturnDate] = useState("");
 
-    const [formattedSelectedDate, setFormattedSelectedDate] = useState('');
-    const [formattedReturnDate, setFormattedReturnDate] = useState('');
+  const flightPort = useSelector((state) => state.passFlightPort.flightPort);
+  const flightPortArrive = useSelector(
+    (state) => state.passFlightPortArrive.flightPortArrive
+  );
+  const passengerInfo = useSelector((state) => state.passInfo.passengerInfo);
+  const selectedDate = useSelector(
+    (state) => state.optionDateDepp.selectedDate
+  );
+  const returnDate = useSelector((state) => state.optionDateArr.returnDate);
+  const refreshPassenger = useSelector(
+    (state) => state.refreshPass.refreshPassenger
+  );
 
-    const flightPort = useSelector((state) => state.passFlightPort.flightPort);
-    const flightPortArrive = useSelector((state) => state.passFlightPortArrive.flightPortArrive);
-    const passengerInfo = useSelector((state) => state.passInfo.passengerInfo);
-    const selectedDate = useSelector((state) => state.optionDateDepp.selectedDate);
-    const returnDate = useSelector((state) => state.optionDateArr.returnDate);
-    const refreshPassenger = useSelector((state) => state.refreshPass.refreshPassenger);
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
 
-    const dispatch = useDispatch();
-    const navigate = useNavigate();
+  const isLeavePort = flightPorts.ports.find(
+    (item) => item.code === flightPort
+  );
+  const isArrivePort = flightPorts.ports.find(
+    (item) => item.code === flightPortArrive
+  );
 
-    const isLeavePort = flightPorts.ports.find((item) => item.code === flightPort);
-    const isArrivePort = flightPorts.ports.find((item) => item.code === flightPortArrive);
+  const leavePortExplanation = isLeavePort ? isLeavePort.explanation : "";
+  const arrivePortExplanation = isArrivePort ? isArrivePort.explanation : "";
 
-    const leavePortExplanation = isLeavePort ? isLeavePort.explanation : '';
-    const arrivePortExplanation = isArrivePort ? isArrivePort.explanation : '';
+  const filteredPorts = passengerInformation.departureLegs.filter((item) => {
+    return item.depPort === flightPort && item.arrPort === flightPortArrive;
+  });
 
-    const filteredPorts = passengerInformation.departureLegs.filter((item) => {
-        return item.depPort === flightPort && item.arrPort === flightPortArrive;
-    });
+  const formatTime = (time) => {
+    const hours = time.getUTCHours();
+    const minutes = time.getUTCMinutes();
+    return `${hours} saat ${minutes} dakika`;
+  };
 
-    const formatTime = (time) => {
-        const hours = time.getUTCHours();
-        const minutes = time.getUTCMinutes();
-        return `${hours} saat ${minutes} dakika`;
-    };
+  const selectedDateTimestamp =
+    selectedDate instanceof Date ? selectedDate.getTime() : null;
 
-    const selectedDateTimestamp = selectedDate instanceof Date ? selectedDate.getTime() : null;
+  useEffect(() => {
+    const selectedDateFormatted =
+      selectedDate instanceof Date ? selectedDate.toDateString() : "";
+    setFormattedSelectedDate(selectedDateFormatted);
 
+    const returnDateFormatted =
+      returnDate instanceof Date ? returnDate.toDateString() : "";
+    setFormattedReturnDate(returnDateFormatted);
+  }, [selectedDate, returnDate]);
 
-    useEffect(() => {
-        const selectedDateFormatted = selectedDate instanceof Date ? selectedDate.toDateString() : '';
-        setFormattedSelectedDate(selectedDateFormatted);
+  const handleMainPageClick = () => {
+    dispatch(setPassengerInfo(refreshPassenger));
+    navigate("/");
+  };
 
-        const returnDateFormatted = returnDate instanceof Date ? returnDate.toDateString() : '';
-        setFormattedReturnDate(returnDateFormatted);
-    }, [selectedDate, returnDate]);
+  const handleTicketClick = (item) => {
+    dispatch(setFlightTicket({ ...item, selectedDate: selectedDateTimestamp }));
+    navigate("/sales-screen");
+  };
 
-    const handleMainPageClick = () => {
-        dispatch(setPassengerInfo(refreshPassenger));
-        navigate('/');
-    };
+  const handleMainPage = () => {
+    dispatch(setPassengerInfo(refreshPassenger));
+    navigate("/");
+  };
 
-    const handleTicketClick = (item) => {
-        dispatch(setFlightTicket({ ...item, selectedDate: selectedDateTimestamp }));
-        navigate('/sales-screen');
-    };
-
-    return (
-        <div className='flyCompanies-container'>
-            <div className='flyCompanies-container__box-info'>
-                <div className='flyCompanies-container__box-info-top'>
-                    <p className='flyCompanies-container__box-info-top-p' onClick={handleMainPageClick}>
-                        YENİDEN UÇUŞ ARA
-                    </p>
-                    <div className='flyCompanies-container__box-info-city'>
-                        <h3>{leavePortExplanation}</h3>
-                        <h3>{arrivePortExplanation}</h3>
-                    </div>
-                    <div className='flyCompanies-container__box-info__date'>
-                        <div className='flyCompanies-container__box-info__date-depp'>
-                            <p>Gidiş</p>
-                            {formattedSelectedDate}
-                        </div>
-                        {formattedReturnDate && (
-                            <div className='flyCompanies-container__box-info__date-return'>
-                                <p>Dönüş</p>
-                                {formattedReturnDate}
-                            </div>
-                        )}
-                    </div>
-                </div>
-                <div className='flyCompanies-container__box-info-bottom'>
-                    <p>{passengerInfo.adults} Yetişkin </p>
-                    {passengerInfo.children > 0 && <p> {`  - ${passengerInfo.children}  Çocuk `}</p>}
-                    {passengerInfo.babies > 0 && <p> {` - ${passengerInfo.babies}  Bebek`} </p>}
-                </div>
+  return (
+    <div>
+      <nav className="navbar">
+        <div className="nav_logo">Some Booking Inc.</div>
+        <ul className="nav_links">
+          <li className="link" onClick={handleMainPage}>
+            <a href="#">Home</a>
+          </li>
+          <li className="link">
+            <a href="#">Book</a>
+          </li>
+          <li className="link">
+            <a href="#">Blog</a>
+          </li>
+          <li className="link">
+            <a href="#">Contact Us</a>
+          </li>
+        </ul>
+      </nav>
+      <div className="flyCompanies-container">
+        <div className="flyCompanies-container__box-info">
+          <div className="flyCompanies-container__box-info-top">
+            <p
+              className="flyCompanies-container__box-info-top-p"
+              onClick={handleMainPageClick}
+            >
+              Yeniden Uçuş Ara
+            </p>
+            <div className="flyCompanies-container__box-info-city">
+              <h3>{leavePortExplanation}</h3>
+              <h3>{arrivePortExplanation}</h3>
             </div>
-            <div className='flyCompanies-container-content'>
-                {filteredPorts.length > 0 ? (
-                    filteredPorts.map((item, key) => {
-                        const depTime = new Date(`1970-01-01T${item.depTime}`);
-                        const arrTime = new Date(`1970-01-01T${item.arrTime}`);
-                        const flightDuration = new Date(arrTime - depTime);
-
-                        return (
-                            <div className='flyCompanies-container-content-container' key={key}>
-                                <div onClick={() => handleTicketClick(item)} className='flyCompanies-container__box'>
-                                    <div className='flyCompanies-container__box-airline'>
-                                        <h4>Havayolu</h4>
-                                        <p>{item.airline}</p>
-                                    </div>
-                                    <div className='flyCompanies-container__box-flightNo'>
-                                        <h4>Kuyruk No</h4>
-                                        <p>{item.flightNo}</p>
-                                    </div>
-                                    <div className='flyCompanies-container__box-depTime'>
-                                        <h4>Kalkış</h4>
-                                        <p>{item.depTime}</p>
-                                    </div>
-                                    <div className='flyCompanies-container__box-flightDuration'>
-                                        <h4>Süre</h4>
-                                        <p>{formatTime(flightDuration)}</p>
-                                    </div>
-                                    <div className='flyCompanies-container__box-arrTime'>
-                                        <h4>Varış</h4>
-                                        <p>{item.arrTime}</p>
-                                    </div>
-                                    <div className='flyCompanies-container__box-amount'>
-                                        <h4>Fiyat(kişi)</h4>
-                                        <p>{item.priceDetail.salesPrice.amount} $</p>
-                                    </div>
-                                </div>
-                            </div>
-                        );
-                    })
-                ) : (
-                    <div className='flyCompanies-container__box-notFound'>
-                        <h2>Uçuş Bulunamadı</h2>
-                    </div>
-                )}
+            <div className="flyCompanies-container__box-info__date">
+              <div className="flyCompanies-container__box-info__date-depp">
+                <p>Gidiş</p>
+                {formattedSelectedDate}
+              </div>
+              {formattedReturnDate && (
+                <div className="flyCompanies-container__box-info__date-return">
+                  <p>Dönüş</p>
+                  {formattedReturnDate}
+                </div>
+              )}
             </div>
+          </div>
+          <div className="flyCompanies-container__box-info-bottom">
+            <p>{passengerInfo.adults} Yetişkin </p>
+            {passengerInfo.children > 0 && (
+              <p> {`  - ${passengerInfo.children}  Çocuk `}</p>
+            )}
+            {passengerInfo.babies > 0 && (
+              <p> {` - ${passengerInfo.babies}  Bebek`} </p>
+            )}
+          </div>
         </div>
-    );
+        <div className="flyCompanies-container-content">
+          {filteredPorts.length > 0 ? (
+            filteredPorts.map((item, key) => {
+              const depTime = new Date(`1970-01-01T${item.depTime}`);
+              const arrTime = new Date(`1970-01-01T${item.arrTime}`);
+              const flightDuration = new Date(arrTime - depTime);
+
+              return (
+                <div
+                  className="flyCompanies-container-content-container"
+                  key={key}
+                >
+                  <div
+                    onClick={() => handleTicketClick(item)}
+                    className="flyCompanies-container__box"
+                  >
+                    <div className="flyCompanies-container__box-airline">
+                      <h4>Havayolu</h4>
+                      <p>{item.airline}</p>
+                    </div>
+                    <div className="flyCompanies-container__box-flightNo">
+                      <h4>Kuyruk No</h4>
+                      <p>{item.flightNo}</p>
+                    </div>
+                    <div className="flyCompanies-container__box-depTime">
+                      <h4>Kalkış</h4>
+                      <p>{item.depTime}</p>
+                    </div>
+                    <div className="flyCompanies-container__box-flightDuration">
+                      <h4>Süre</h4>
+                      <p>{formatTime(flightDuration)}</p>
+                    </div>
+                    <div className="flyCompanies-container__box-arrTime">
+                      <h4>Varış</h4>
+                      <p>{item.arrTime}</p>
+                    </div>
+                    <div className="flyCompanies-container__box-amount">
+                      <h4>Fiyat(kişi)</h4>
+                      <p>{item.priceDetail.salesPrice.amount} $</p>
+                    </div>
+                  </div>
+                </div>
+              );
+            })
+          ) : (
+            <div className="flyCompanies-container__box-notFound">
+              <h2>Uçuş Bulunamadı</h2>
+            </div>
+          )}
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default FlyCompanies;
--- src/components/content/shoppingSummary/index.jsx
+++ src/components/content/shoppingSummary/index.jsx
@@ -1,95 +1,111 @@
-import React, { useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import { useNavigate, } from 'react-router-dom';
-import { setPassName, setPassengerInfo } from '../../configure';
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
+import { setPassName, setPassengerInfo } from "../../configure";
 
-import './index.scss';
+import "./index.scss";
 
 function ShoppingSummary() {
-    const flightTicket = useSelector((state) => state.passTicket.flightTicket);
-    const passengerInfo = useSelector((state) => state.passInfo.passengerInfo);
-    const passSurname = useSelector((state) => state.passCheck.passSurname);
-    const passName = useSelector((state) => state.passCheck.passName);
-    const refreshPassenger = useSelector((state) => state.refreshPass.refreshPassenger);
+  const flightTicket = useSelector((state) => state.passTicket.flightTicket);
+  const passengerInfo = useSelector((state) => state.passInfo.passengerInfo);
+  const passSurname = useSelector((state) => state.passCheck.passSurname);
+  const passName = useSelector((state) => state.passCheck.passName);
+  const refreshPassenger = useSelector(
+    (state) => state.refreshPass.refreshPassenger
+  );
 
-    const navigate = useNavigate();
-    const dispatch = useDispatch()
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
 
+  console.log(passName);
 
-    console.log(passName);
+  const totalPassenger = sessionStorage.getItem("totalPassenger");
+  const totalPrice = flightTicket.priceDetail.basePrice.amount * totalPassenger;
 
-    const totalPassenger = sessionStorage.getItem('totalPassenger');
-    const totalPrice = flightTicket.priceDetail.basePrice.amount * totalPassenger;
+  const logo =
+    "https://uploads-ssl.webflow.com/605c9d764f1ef938a009ac98/61e01bfbdd8632a72962edc2_Pinsoft_Yatay_Logo_mavi-for%20animation.svg";
 
-    const logo =
-        'https://uploads-ssl.webflow.com/605c9d764f1ef938a009ac98/61e01bfbdd8632a72962edc2_Pinsoft_Yatay_Logo_mavi-for%20animation.svg';
+  const handleMainPage = () => {
+    dispatch(setPassengerInfo(refreshPassenger));
+    navigate("/");
+  };
 
-    const handleMainPage = () => {
-        dispatch(setPassengerInfo(refreshPassenger))
-        navigate('/');
-    };
-
-    return (
-        <div className='shoppingSummary-container'>
-            <div className='shoppingSummary__container-navbar'>
-                <nav>
-                    <img onClick={handleMainPage} src={logo} />
-                </nav>
-                <div className='shoppingSummary__container-navbar-alt'>
-                    <h3>Özet</h3>
-                </div>
-            </div>
-            <div className='shoppingSummary-container-box'>
-                <h4 onClick={handleMainPage}>Anasayfaya Dön</h4>
-                <div className='shoppingSummary-container-box__list'>
-                    <div className='shoppingSummary-container-box__list-top'>
-                        <p>{flightTicket.airline}</p>
-                        <p>{flightTicket.flightNo}</p>
-                        <p>{flightTicket.depTime}</p>
-                        <p>{flightTicket.arrTime}</p>
-                        <p> Toplam: {totalPassenger} Yolcu </p>
-                    </div>
-
-                    <div className='shoppingSummary-container-box__list-bottom'>
-                        <div className='shoppingSummary-container-box__list-name' >
-                            <div className='name' >
-                                {passName.map((name, index) => (
-                                    <div className='name-flex' >
-                                        <p>Adı:</p>
-                                        <p key={index}> {name}</p>
-                                    </div>
-                                ))}
-                            </div>
-                        </div>
-                        <div className='shoppingSummary-container-box__list-surName' >
-                            <div className='surname'  >
-                                {passSurname.map((surname, index) => (
-                                    <div className='surname-flex' >
-                                        <p>Soyadı:</p>
-                                        <p key={index}> {surname}</p>
-                                    </div>
-                                ))}
-                            </div>
-                        </div>
-
-                    </div>
-                    <div className='shoppingSummary-container-box__list-passenger' >
-                        <p>{totalPrice} $</p>
-                        <div className='shoppingSummary-container-box__list-passengerInfo' >
-                            <p>{passengerInfo.adults} Yetişkin </p>
-                            {passengerInfo.children > 0 && (
-                                <p> {`  - ${passengerInfo.children}  Çocuk `}</p>
-                            )}
-                            {passengerInfo.babies > 0 && (
-                                <p> {` - ${passengerInfo.babies}  Bebek`} </p>
-                            )}
-
-                        </div>
-                    </div>
-                </div>
-            </div>
+  return (
+    <div className="shoppingSummary-container">
+      <div className="shoppingSummary__container-navbar">
+        {/* <nav>
+          <img onClick={handleMainPage} src={logo} />
+  </nav> */}
+        <nav className="navbar">
+          <div className="nav_logo">Some Booking Inc.</div>
+          <ul className="nav_links">
+            <li className="link" onClick={handleMainPage}>
+              <a href="#">Home</a>
+            </li>
+            <li className="link">
+              <a href="#">Book</a>
+            </li>
+            <li className="link">
+              <a href="#">Blog</a>
+            </li>
+            <li className="link">
+              <a href="#">Contact Us</a>
+            </li>
+          </ul>
+        </nav>
+        <div className="shoppingSummary__container-navbar-alt">
+          <h3>Uçuş Bilgileri Özeti</h3>
         </div>
-    );
+      </div>
+      <div className="shoppingSummary-container-box">
+        <div className="shoppingSummary-container-box__list">
+          <div className="shoppingSummary-container-box__list-top">
+            <p>{flightTicket.airline}</p>
+            <p>{flightTicket.flightNo}</p>
+            <p>{flightTicket.depTime}</p>
+            <p>{flightTicket.arrTime}</p>
+            <p> Toplam: {totalPassenger} Yolcu </p>
+          </div>
+
+          <div className="shoppingSummary-container-box__list-bottom">
+            <div className="shoppingSummary-container-box__list-name">
+              <div className="name">
+                {passName.map((name, index) => (
+                  <div className="name-flex">
+                    <p className="passenger-name">Adı:</p>
+                    <p key={index}> {name}</p>
+                  </div>
+                ))}
+              </div>
+            </div>
+            <div className="shoppingSummary-container-box__list-surName">
+              <div className="surname">
+                {passSurname.map((surname, index) => (
+                  <div className="surname-flex">
+                    <p className="passenger-name">Soyadı:</p>
+                    <p key={index}> {surname}</p>
+                  </div>
+                ))}
+              </div>
+            </div>
+          </div>
+          <div className="shoppingSummary-container-box__list-passenger">
+            <p>{totalPrice} $</p>
+            <div className="shoppingSummary-container-box__list-passengerInfo">
+              <p>{passengerInfo.adults} Yetişkin </p>
+              {passengerInfo.children > 0 && (
+                <p> {`  - ${passengerInfo.children}  Çocuk `}</p>
+              )}
+              {passengerInfo.babies > 0 && (
+                <p> {` - ${passengerInfo.babies}  Bebek`} </p>
+              )}
+            </div>
+          </div>
+        </div>
+        <h5 onClick={handleMainPage}>Anasayfaya Dön</h5>
+      </div>
+    </div>
+  );
 }
 
 export default ShoppingSummary;
