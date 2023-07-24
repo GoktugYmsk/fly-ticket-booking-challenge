b78d261bc6d2a37ca7b2edbd498186981300137d
serdaryld <serdaryildrm21@gmail.com>
2023-07-24 18:17:43 +0300

    style edit

--- src/components/content/searchContent/searchItem3/index.jsx
+++ src/components/content/searchContent/searchItem3/index.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function SearchItem3() {
     return (
         <div>
-            <p>Control Your Flight</p>
+            <p>Manage Your Flight</p>
         </div>
     )
 }
