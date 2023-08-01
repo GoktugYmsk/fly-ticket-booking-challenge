c578ad1bfb42f997d449cfe5cc79d4a79a4db58f
GoktugYmsk <gogtug033@outlook.com>
2023-08-01 10:14:37 +0300

    bugs are fixed

--- src/components/salesScreen/index.jsx
+++ src/components/salesScreen/index.jsx
@@ -4,37 +4,18 @@ import { useDispatch, useSelector } from 'react-redux';
 
 import Header from "../header";
 import MyImage from './arrow.png';
-
-import { setPassName, setPassSurname } from '../configure';
+import RenderPassengerDetails from './renderPassengerDetails';
 
 import './index.scss';
 
 function SalesScreen() {
-    const passName = useSelector((state) => state.passCheck.passName);
-    const passSurname = useSelector((state) => state.passCheck.passSurname);
     const flightTicket = useSelector((state) => state.passTicket.flightTicket);
-    const passengerInfo = useSelector((state) => state.passInfo.passengerInfo);
 
     const totalPassenger = sessionStorage.getItem('totalPassenger');
 
-    const dispatch = useDispatch();
     const navigate = useNavigate();
 
     const imagePath = MyImage;
-
-    const handleNameChange = (e, passengerIndex) => {
-        const newName = e.target.value;
-        const currentPassNames = [...passName];
-        currentPassNames[passengerIndex - 1] = newName;
-        dispatch(setPassName(currentPassNames));
-    };
-
-    const handleSurnameChange = (e, passengerIndex) => {
-        const newSurname = e.target.value;
-        const currentPassSurnames = [...passSurname];
-        currentPassSurnames[passengerIndex - 1] = newSurname;
-        dispatch(setPassSurname(currentPassSurnames));
-    }
 
     const handlePayScreenClick = () => {
         navigate('/seat-screen');
@@ -42,52 +23,6 @@ function SalesScreen() {
 
     const totalPrice = flightTicket.passengerPrices[0].basePrice * totalPassenger;
 
-    const renderPassengerDetails = () => {
-        const passengerDetails = [];
-        const passengerCounts = [passengerInfo.adults, passengerInfo.children, passengerInfo.babies];
-
-        for (let i = 0; i < passengerCounts.length; i++) {
-            for (let j = 1; j <= passengerCounts[i]; j++) {
-                const passengerIndex = passengerCounts.slice(0, i).reduce((acc, count) => acc + count, 0) + j;
-                let passengerType = '';
-                switch (i) {
-                    case 0:
-                        passengerType = 'Adult';
-                        break;
-                    case 1:
-                        passengerType = 'Child';
-                        break;
-                    case 2:
-                        passengerType = 'Infant';
-                        break;
-                    default:
-                        break;
-                }
-
-                passengerDetails.push(
-                    <div key={passengerIndex} className='flight-contanier-box'>
-                        <div className='passenger-leftInfo'>
-                            <h4>{`${j}. ${passengerType}`}</h4>
-                        </div>
-                        <div className="flight-box-name">
-                            <div className="no"><input placeholder="First Name" onChange={(e) => handleNameChange(e, passengerIndex)} required /></div>
-                        </div>
-                        <div className="flight-box-surname">
-                            <div className="no"><input placeholder="Last Name" onChange={(e) => handleSurnameChange(e, passengerIndex)} required /></div>
-                        </div>
-                        <div className="flight-box-id">
-                            <div className="time"><input placeholder="ID Number" type="text" /></div>
-                        </div>
-                        <div className="flight-box-birth">
-                            <div className="arrive-title">Birth Date:</div>
-                            <div className="arrive"><input placeholder="Gidiş tarihini seçiniz." type="date" name="birth-date" /></div>
-                        </div>
-                    </div>
-                );
-            }
-        }
-        return passengerDetails;
-    };
 
     return (
         <>
@@ -122,9 +57,7 @@ function SalesScreen() {
             </div>
             <div className='salesScreen-container-content__box__invoice'>
                 <h3>Passenger Information</h3>
-                <div className='flight-passenger-container'>
-                    {renderPassengerDetails()}
-                </div>
+                <RenderPassengerDetails />
                 <h3>Contact Information</h3>
                 <div className="flight-larger-container-contact">
                     <div className="flight-box-no">
