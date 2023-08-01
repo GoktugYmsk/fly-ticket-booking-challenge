ceba4104eb8acd2539ca940499de6d421183e254
GoktugYmsk <gogtug033@outlook.com>
2023-08-01 10:51:42 +0300

    bug are fixed

--- src/components/salesScreen/renderPassengerDetails/index.jsx
+++ src/components/salesScreen/renderPassengerDetails/index.jsx
@@ -1,79 +1,87 @@
-import React from 'react'
 import { useDispatch, useSelector } from 'react-redux';
 import { setPassName, setPassSurname } from '../../configure';
 
-function RenderPassengerDetails() {
+function PassengerDetailBox({ passengerIndex, passengerType }) {
+    const dispatch = useDispatch();
     const passName = useSelector((state) => state.passCheck.passName);
     const passSurname = useSelector((state) => state.passCheck.passSurname);
-    const passengerInfo = useSelector((state) => state.passInfo.passengerInfo);
 
-    const dispatch = useDispatch()
-
-    const handleNameChange = (e, passengerIndex) => {
+    const handleNameChange = (e) => {
         const newName = e.target.value;
         const currentPassNames = [...passName];
         currentPassNames[passengerIndex - 1] = newName;
         dispatch(setPassName(currentPassNames));
     };
 
-    const handleSurnameChange = (e, passengerIndex) => {
+    const handleSurnameChange = (e) => {
         const newSurname = e.target.value;
         const currentPassSurnames = [...passSurname];
         currentPassSurnames[passengerIndex - 1] = newSurname;
         dispatch(setPassSurname(currentPassSurnames));
-    }
-
-    const renderPassengerDetails = () => {
-        const passengerDetails = [];
-        const passengerCounts = [passengerInfo.adults, passengerInfo.children, passengerInfo.babies];
-
-        for (let i = 0; i < passengerCounts.length; i++) {
-            for (let j = 1; j <= passengerCounts[i]; j++) {
-                const passengerIndex = passengerCounts.slice(0, i).reduce((acc, count) => acc + count, 0) + j;
-                let passengerType = '';
-                switch (i) {
-                    case 0:
-                        passengerType = 'Adult';
-                        break;
-                    case 1:
-                        passengerType = 'Child';
-                        break;
-                    case 2:
-                        passengerType = 'Infant';
-                        break;
-                    default:
-                        break;
-                }
-
-                passengerDetails.push(
-                    <div key={passengerIndex} className='flight-contanier-box'>
-                        <div className='passenger-leftInfo'>
-                            <h4>{`${j}. ${passengerType}`}</h4>
-                        </div>
-                        <div className="flight-box-name">
-                            <div className="no"><input placeholder="First Name" onChange={(e) => handleNameChange(e, passengerIndex)} required /></div>
-                        </div>
-                        <div className="flight-box-surname">
-                            <div className="no"><input placeholder="Last Name" onChange={(e) => handleSurnameChange(e, passengerIndex)} required /></div>
-                        </div>
-                        <div className="flight-box-id">
-                            <div className="time"><input placeholder="ID Number" type="text" /></div>
-                        </div>
-                        <div className="flight-box-birth">
-                            <div className="arrive-title">Birth Date:</div>
-                            <div className="arrive"><input placeholder="Gidiş tarihini seçiniz." type="date" name="birth-date" /></div>
-                        </div>
-                    </div>
-                );
-            }
-        }
-        return passengerDetails;
     };
+
     return (
-        <div className='flight-passenger-container'>
-            {renderPassengerDetails()}
+        <div key={passengerIndex} className='flight-contanier-box'>
+            <div className='passenger-leftInfo'>
+                <h4>{`${passengerIndex}. ${passengerType}`}</h4>
+            </div>
+            <div className="flight-box-name">
+                <div className="no">
+                    <input placeholder="First Name" onChange={handleNameChange} required />
+                </div>
+            </div>
+            <div className="flight-box-surname">
+                <div className="no">
+                    <input placeholder="Last Name" onChange={handleSurnameChange} required />
+                </div>
+            </div>
+            <div className="flight-box-id">
+                <div className="time">
+                    <input placeholder="ID Number" type="text" />
+                </div>
+            </div>
+            <div className="flight-box-birth">
+                <div className="arrive-title">Birth Date:</div>
+                <div className="arrive">
+                    <input placeholder="Gidiş tarihini seçiniz." type="date" name="birth-date" />
+                </div>
+            </div>
         </div>
-    )
+    );
 }
 
-export default RenderPassengerDetails
+function RenderPassengerDetails() {
+    const passengerInfo = useSelector((state) => state.passInfo.passengerInfo);
+    const passengerCounts = [passengerInfo.adults, passengerInfo.children, passengerInfo.babies];
+
+    const passengerDetails = [];
+    for (let i = 0; i < passengerCounts.length; i++) {
+        for (let j = 1; j <= passengerCounts[i]; j++) {
+            let passengerIndex = passengerCounts.slice(0, i).reduce((acc, count) => acc + count, 0) + j;
+            let passengerType = '';
+            switch (i) {
+                case 0:
+                    passengerType = 'Adult';
+                    break;
+                case 1:
+                    passengerType = 'Child';
+                    break;
+                case 2:
+                    passengerType = 'Infant';
+                    break;
+                default:
+                    break;
+            }
+
+            passengerDetails.push(<PassengerDetailBox key={passengerIndex} passengerIndex={passengerIndex} passengerType={passengerType} />);
+        }
+    }
+
+    return (
+        <div className='flight-passenger-container'>
+            {passengerDetails}
+        </div>
+    );
+}
+
+export default RenderPassengerDetails;
--- src/index.js
+++ src/index.js
@@ -1,4 +1,3 @@
-import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
@@ -6,6 +5,9 @@ import { Provider } from 'react-redux';
 import { store } from './app/store';
 import { BrowserRouter } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import 'core-js/stable';
+import 'regenerator-runtime/runtime';
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
